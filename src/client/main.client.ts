22c33e8010ed8a0f2aabea7ec51481128aa5a02f
G. van Dorland <giodor13@gmail.com>
2025-08-05 23:53:53 +0200

    Update main.client.ts

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,1 +1,11 @@
 
+import React from "@rbxts/react";
+
+import { createRoot } from "@rbxts/react-roblox";
+import { Players } from "@rbxts/services";
+
+
+const sg = Players.LocalPlayer.WaitForChild("PlayerGui").FindFirstChild("ScreenGui") ?? new Instance("ScreenGui");
+sg.Parent = Players.LocalPlayer.WaitForChild("PlayerGui")
+const root = createRoot(sg);
+root.render(<frame Size={new UDim2(1,0,1,0)} />);
